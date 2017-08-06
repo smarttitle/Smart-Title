73a23d25eb511cc2b005d409e0b4a725c13b77fc
Sun-woo, Lee <eldkqmfhf@gmail.com>
2017-08-06 15:07:29 +0900

    Evaluating, Improving, and Tuning the ANN Part 3

--- Study/Artificial-Neural-Networks/Sunwoo/Building an ANN.py
+++ Study/Artificial-Neural-Networks/Sunwoo/Building an ANN.py
@@ -114,3 +114,27 @@ variance = accuracies.std()
 # Improving the ANN
 # Dropout Regulization to reduce ovefitting if needed
 
+# Tuning the ANN
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.grid_search import GridSearchCV
+from keras.models import Sequential
+from keras.layers import Dense
+def build_classifier(optimizer):
+    classifier = Sequential()
+    classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu', input_dim=11))
+    classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu'))
+    classifier.add(Dense(units=1, kernel_initializer='uniform', activation='sigmoid'))
+    classifier.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
+    return classifier
+
+classifier = KerasClassifier(build_fn=build_classifier)
+parameters = {'batch_size': [25, 32],
+              'epochs': [100, 500],
+              'optimizer': ['adam', 'rmsprop']} 
+grid_search = GridSearchCV(estimator = classifier,
+                           param_grid = parameters,
+                           scoring = 'accuracy',
+                           cv = 10)
+grid_search = grid_search.fit(X_train, y_train)
+best_parameters = grid_search.best_params_
+best_accuracy = grid_search.best_score_
