99a92553dbc03ca7259c7dddc30645b3f737774f
Sun-woo, Lee <eldkqmfhf@gmail.com>
2017-08-06 13:12:13 +0900

    Evaluating, Improving, and Tuning the ANN Part 1

--- Study/Artificial-Neural-Networks/Sunwoo/Building an ANN.py
+++ Study/Artificial-Neural-Networks/Sunwoo/Building an ANN.py
@@ -91,3 +91,22 @@ from sklearn.metrics import confusion_matrix
 
 cm = confusion_matrix(y_test, y_pred)
 
+# Part 4 - Evaluating, Improving and Tuning the ANN
+
+# Evaluating the ANN
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+from keras.models import Sequential
+from keras.layers import Dense
+def build_classifier():
+    classifier = Sequential()
+    classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu', input_dim=11))
+    classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu'))
+    classifier.add(Dense(units=1, kernel_initializer='uniform', activation='sigmoid'))
+    classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    return classifier
+
+classifier = KerasClassifier(build_fn=build_classifier, batch_size = 10, epochs=100)
+accuracies = cross_val_score(estimator=classifier, X = X_train, y = y_train, cv = 10, n_jobs=-1)
+mean = accuracies.mean()
+variance = accuracies.std()
