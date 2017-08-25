8f1d3521500f23e81a9f9efe3ce16a0fb0c9b07d
ChoongHoon Lee <ch930701@gmail.com>
2017-08-25 18:41:22 +0900

    Natural Language Toolkit
    NLTK tutorial step1. Tokenizing Words and Sentences with NLTK

---
+++ Study/NLTK/ChoongHoon/Toknizing.py
@@ -0,0 +1,5 @@
+from nltk import sent_tokenize, word_tokenize
+example_sentence = "The man who always tell the truth never have to remember anything."
+print(sent_tokenize(example_sentence))
+
+print(word_tokenize(example_sentence))
